6568b82928f32488b720d0b1a60e8e89d6f50693
EdLoth <eduardosram10@gmail.com>
2024-12-23 17:30:26 -0300

    🌟feat: component header

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,8 +4,8 @@ import { usePathname, useRouter } from "next/navigation";
 import { NavButton } from "./nav-button";
 
 import { useMedia } from 'react-use'
-import { 
-  Sheet, 
+import {
+  Sheet,
   SheetContent,
   SheetTrigger
 } from '@/components/ui/sheet'
@@ -53,9 +53,9 @@ export const Navigation = () => {
       <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger>
           <Button
-          variant="outline"
-          size="sm"
-          className="
+            variant="outline"
+            size="sm"
+            className="
           font-normal bg-white/10 hover:bg-white/20 
           hover:text-white border-none focus-visible:ring-offset-0
           focus-visible:ring-transparent outline-none text-white
@@ -66,18 +66,27 @@ export const Navigation = () => {
           </Button>
         </SheetTrigger>
         <SheetContent side="left" className="px-2">
-        <nav className="flex flex-col gap-y-2 pt-6">
-
-        </nav>
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                key={route.href}
+                variant={route.href === pathname ? "secondary" : "ghost"}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
         </SheetContent>
       </Sheet>
-    )
-  }
+    );
+  };
 
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
       {routes.map((route) => (
-        <NavButton 
+        <NavButton
           key={route.href}
           href={route.href}
           label={route.label}
