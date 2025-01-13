ca7c1e53e9d5c9da813d3c343a6528ea1686e57d
Eduardo Ramos <eduardosram10@gmail.com>
2025-01-13 15:23:06 -0300

    feat: add favicon

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,17 @@
 import { Header } from "@/components/Header";
+import { Metadata } from "next";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
 }
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/logo.svg', // Verifique se o caminho está correto
+  },
+};
+
 const DashboardLayout = ({ children }: Props) => {
   return (
     <>
